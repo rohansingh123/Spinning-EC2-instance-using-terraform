391c1177dcb10080f59e67a7ff5f4aa149d50994
Vigyat Tripathi <32060077+vigyat123@users.noreply.github.com>
2018-07-03 00:01:47 +0530

    Create Spinning_EC2_Using_terraform.tf

---
+++ Spinning_EC2_Using_terraform.tf
@@ -0,0 +1,18 @@
+#Spinning_EC2_Using_terraform
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+provider "aws" {
+        access_key = "${var.AWS_ACCESS_KEY}"
+        secret_key = "${var.AWS_SECRET_KEY}"
+        region = "us-east-1d"
+}
+
+resource "aws_instance" "example" {
+        ami = "ami-id"
+        instance_type = "t1.micro"
+        key_name = "Key_Pair"
+        security_groups= ["security_Group"]
+        tags {
+         Name = "terraform-instance"
+        }
+}
