269fd9dbf00527ba697d2d495814af669790e450
Vigyat Tripathi <32060077+vigyat123@users.noreply.github.com>
2018-07-03 00:27:48 +0530

    Update Spinning_EC2_Using_terraform.tf

--- Spinning_EC2_Using_terraform.tf
+++ Spinning_EC2_Using_terraform.tf
@@ -1,6 +1,6 @@
-#Spinning_EC2_Using_terraform
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+#Spinning_EC2_Using_terraform.tf
+variable "AWS_ACCESS_KEY" {} #pick value from terraform.tfvars
+variable "AWS_SECRET_KEY" {} #pick value from terraform.tfvars
 provider "aws" {
         access_key = "${var.AWS_ACCESS_KEY}"
         secret_key = "${var.AWS_SECRET_KEY}"
